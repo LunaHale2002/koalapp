476e6e8ac45330e506006866ec0393c430467c26
LunaHale2002 <lbabic@edenschool.fr>
2024-03-12 17:15:22 +0100

    Koalapp

---
+++ main.js
@@ -0,0 +1,11 @@
+// import './style.css';
+document.body.innerHTML = 
+`
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">New Question</a></li>
+            <li><a href="#">Leader Board</a></li>
+        </ul>
+    </nav>
+`;
